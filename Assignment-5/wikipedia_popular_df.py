bfd54ce0e2908fa8c9f0dc6d7f61457d12550d7a
ehoque <ehoque@asb10928u-b04.csil.sfu.ca>
2018-10-10 15:31:40 -0700

    wiki final

--- Assignment-5/wikipedia_popular_df.py
+++ Assignment-5/wikipedia_popular_df.py
@@ -7,20 +7,27 @@ assert spark.version >= '2.3' # make sure we have Spark 2.3+
 
 
 wiki_schema = types.StructType([ # commented-out fields won't be read
+     types.StructField('hour', types.StringType(), True),
     types.StructField('lang', types.StringType(), True),
-    types.StructField('key', types.StringType(), True),
-    types.StructField('hit', types.LongType(), True),
+    types.StructField('title', types.StringType(), True),
+    types.StructField('views', types.LongType(), True),
     types.StructField('size', types.LongType(), True)
 ])
 
+def path_to_hour(path):
+    result = f.split(path,'/')
+    file_name = f.split(result[f.size(result)-1],'pagecounts-')
+    return file_name[f.size(file_name)-1].substr(1,11)
+
 
 def main(inputs, output):
-    data = spark.read.csv(inputs, schema=wiki_schema, sep = ' ')
-    new_frame = weather.filter(weather.qflag.isNull()).filter(weather.station.startswith('CA')).filter(weather.observation == 'TMAX')
-	# cnt = comments.groupBy(comments.subreddit).agg(f.count(comments.subreddit).alias('count'),f.sum(comments.score).alias('sum'))
-	# avg = cnt.withColumn('average', cnt['sum'] / cnt['count'] )
-	# result = avg[['subreddit','average']]
-	# result.write.csv(output, mode='overwrite')
+    data = spark.read.csv(inputs, schema=wiki_schema, sep = ' ').withColumn("hour", path_to_hour(f.input_file_name()))
+    new_frame = data.filter(data.lang == 'en').filter(data.title != 'Main_Page').filter(data.title.contains('Special:') == False).cache()
+    maxs = new_frame.groupBy(new_frame.hour).agg(f.max(new_frame.views).alias('mx')).alias('maxs')
+    result=new_frame.join(maxs,(maxs.hour == new_frame.hour) & (new_frame.views == maxs.mx)).select(new_frame.hour, new_frame.title, new_frame.views).drop(maxs.mx).sort(new_frame.hour, new_frame.title).drop_duplicates()
+    result.write.json(output, mode='overwrite')
+    #result.show(n=100)
+   
 
 if __name__ == '__main__':
     inputs = sys.argv[1]
