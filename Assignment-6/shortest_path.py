32e7d6412251c3f7b6179585ebb4e50941d2b6a9
EkramulHoque <ekramulhq26@gmail.com>
2018-10-19 23:47:08 -0700

    final

--- Assignment-6/shortest_path.py
+++ Assignment-6/shortest_path.py
@@ -1,7 +1,6 @@
 from pyspark import SparkConf, SparkContext
 import sys
-import math
-assert sys.version_info >= (3, 5) # make sure we have Python 3.5+
+assert sys.version_info >= (3, 5)  # make sure we have Python 3.5+
 from pyspark.sql import SQLContext, functions
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, ArrayType
 
@@ -22,8 +21,8 @@ def f(x):
     return x
 
 def make_known_path(kv):
-    k,v = kv
-    return (k,(v.split()))
+    k, v = kv
+    return (k, (v.split()))
 
 def word_split(line):
     split_words = line.split(':')
@@ -32,35 +31,42 @@ def word_split(line):
 def filter_len(line):
     return len(line) > 1
 
-def main(inputs,output,source,destination):
+def debug_result(x):
+    return "node %s: source %s, distance %i" % (x[0], x[1], x[2])
+
+def main(inputs, output, source, destination):
     text = sc.textFile(inputs)
-    #split the line for nodes and its adjacent neighbours and create a pair with each pair ex [1,(3,5)] => [(1,3),(1,5)]
     graph_edges = text.map(word_split).filter(filter_len).map(make_known_path).flatMapValues(f)
-    #create the table as (source,destination)
     graph_edges = sqlContext.createDataFrame(graph_edges, schema).cache()
-    #create the table as (node,(source,distance))
-    known_paths = sc.parallelize([[source,'No Source', 0]])
-    known_paths = sqlContext.createDataFrame(known_paths,schema2).cache()
+    known_paths = sc.parallelize([[source, 'No Source', 0]])
+    known_paths = sqlContext.createDataFrame(known_paths, schema2).cache()
     next_node = sc.parallelize([[source]])
-    next_node = sqlContext.createDataFrame(next_node,schema3).cache()
-    #get the neighbours of a single node
+    next_node = sqlContext.createDataFrame(next_node, schema3).cache()
     for i in range(6):
-	    neighbours = graph_edges.join(next_node, graph_edges.source == next_node.node).drop('node').cache()
-	    new_paths = known_paths.join(neighbours, known_paths.node == neighbours.source).select(neighbours.destination,neighbours.source,known_paths.distance)
-	    new_paths = new_paths.withColumn('new_dist', new_paths.distance + 1).drop(new_paths.distance).cache()
-	    # union new paths and known paths
-	    known_paths = known_paths.unionAll(new_paths).dropDuplicates().cache()
-	    min_paths = known_paths.groupBy(known_paths.node).agg(functions.min(known_paths.distance).alias('min_distance')).withColumnRenamed('node','min_node')
-	    known_paths =  min_paths.join(known_paths,(min_paths.min_node == known_paths.node) & (min_paths.min_distance == known_paths.distance)).drop('min_node','min_distance').cache()
-	    #set next node
-	    next_node = neighbours.select('source').withColumnRenamed('source','node')
-	    if next_node[next_node.node == destination].count() > 0 :
-	    	break
+        neighbours = graph_edges.join(next_node, graph_edges.source == next_node.node).drop('node').cache()
+        new_paths = known_paths.join(neighbours, known_paths.node == neighbours.source).select(neighbours.destination,
+                                                                                               neighbours.source,
+                                                                                               known_paths.distance)
+        new_paths = new_paths.withColumn('new_dist', new_paths.distance + 1).drop(new_paths.distance).cache()
+        known_paths = known_paths.unionAll(new_paths).dropDuplicates().cache()
+        min_paths = known_paths.groupBy(known_paths.node).agg(
+            functions.min(known_paths.distance).alias('min_distance')).withColumnRenamed('node', 'min_node')
+        known_paths = min_paths.join(known_paths, (min_paths.min_node == known_paths.node) & (
+                    min_paths.min_distance == known_paths.distance)).drop('min_node', 'min_distance').cache()
+        next_node = neighbours.select('destination').withColumnRenamed('destination', 'node')
+        debug = known_paths.rdd.map(debug_result)
+        debug.saveAsTextFile(output + '/iter-' + str(i))
+        if next_node[next_node.node == destination].count() > 0:
+            break
     known_paths.show()
-
-    
-   
-
+    outdata = []
+    path = destination
+    outdata.insert(0, path)
+    while (path != source):
+        path = known_paths[known_paths.node == path].select('source').first()[0]
+        outdata.insert(0, path)
+    outdata = sc.parallelize(outdata)
+    outdata.saveAsTextFile(output + '/path')
 
 if __name__ == '__main__':
     conf = SparkConf().setAppName('nasa-log-ingest')
@@ -71,4 +77,4 @@ if __name__ == '__main__':
     output = sys.argv[2]
     source = sys.argv[3]
     destination = sys.argv[4]
-    main(inputs,output,source,destination)
+    main(inputs, output, source, destination)
