6d59f729f3f4a81b31a56b87186c631681bd3e78
ehoque <ehoque@asb10928u-d05.csil.sfu.ca>
2018-11-05 20:16:07 -0800

    weather-train

--- Assignment-9/weather_train.py
+++ Assignment-9/weather_train.py
@@ -10,7 +10,7 @@ assert spark.version >= '2.3'  # make sure we have Spark 2.3+
 
 from pyspark.ml import Pipeline
 from pyspark.ml.feature import StringIndexer, VectorAssembler, SQLTransformer
-from pyspark.ml.regression import AFTSurvivalRegression
+from pyspark.ml.regression import GBTRegressor
 from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.sql.functions import dayofyear
 
@@ -23,26 +23,31 @@ tmax_schema = types.StructType([
     types.StructField('tmax', types.FloatType()),
 ])
 
-def main(inputs):
+def main(inputs,model_file):
     data = spark.read.csv(inputs, schema=tmax_schema)
     train, validation = data.randomSplit([0.75, 0.25])
     train = train.cache()
     validation = validation.cache()
     
     sqlTrans = SQLTransformer(statement="""SELECT *, dayofyear( date ) AS day FROM __THIS__""")
-    evaluator = RegressionEvaluator().setLabelCol("tmax").setPredictionCol("tmax_pred")
     weather_assembler = VectorAssembler(inputCols=["latitude", "longitude", "elevation", "day"], outputCol="features")
-    estimator = AFTSurvivalRegression(featuresCol='features', labelCol='tmax', predictionCol='tmax_pred')
-    pipeline = Pipeline(stages=[sqlTrans, weather_assembler, estimator])
+    estimator = GBTRegressor()
+    word_indexer = StringIndexer(inputCol="station", outputCol="label", handleInvalid='error')
+    pipeline = Pipeline(stages=[sqlTrans, weather_assembler,word_indexer, estimator])
+    evaluator = RegressionEvaluator()
 
     model = pipeline.fit(train)
     predictions = model.transform(validation)
-    score = evaluator.evaluate(predictions)
+    r2 = evaluator.evaluate(predictions,{evaluator.metricName: "r2"})
+    rmse = evaluator.evaluate(predictions,{evaluator.metricName: "rmse"})
+    model.write().overwrite().save(model_file)
     print("==============================================")
-    print('Validation Score On Weather Model' + str(score))
+    print('Root Square : ' + str(r2))
+    print('Root Mean Square : ' + str(rmse))
     print("==============================================")
 
 
 if __name__ == '__main__':
     inputs = sys.argv[1]
-    main(inputs)
+    model_file = sys.argv[2]
+    main(inputs,model_file)
