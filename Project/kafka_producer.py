80bc9b0ccf0f7b6a9f8bc736a2e8e80a925a4edf
EkramulHoque <ekramulhq26@gmail.com>
2018-11-09 02:00:10 -0800

    customer generator, kafka producer

--- Project/kafka_producer.py
+++ Project/kafka_producer.py
@@ -1,128 +1,27 @@
-import threading,logging,time,json,sys
-assert sys.version_info >= (3, 5)
-from kafka import KafkaConsumer, KafkaProducer
-
-json_file = [
-   {
-      "_id": "5be0a0504817b660eef107f2",
-      "CustID": 0,
-      "balance": "$11.53",
-      "phone": "+1 (825) 454-2369",
-      "Location": "Ontario",
-      "Start": "2018-10-12T05:45:24",
-      "End": "2018-10-12T05:56:42",
-      "Event": {
-         "Voice": True,
-         "Data": False
-      }
-   },
-   {
-      "_id": "5be0a050faa7b8165c643095",
-      "CustID": 1,
-      "balance": "$3.38",
-      "phone": "+1 (830) 572-3479",
-      "Location": "Manitoba",
-      "Start": "2018-06-06T12:05:25",
-      "End": "2018-06-06T12:08:45",
-      "Event": {
-         "Voice": False,
-         "Data": True
-      }
-   },
-   {
-      "_id": "5be0a050e4fb083e93386167",
-      "CustID": 2,
-      "balance": "$4.46",
-      "phone": "+1 (929) 415-2591",
-      "Location": "Manitoba",
-      "Start": "2018-11-08T08:06:23",
-      "End": "2018-11-08T08:10:23",
-      "Event": {
-         "Voice": True,
-         "Data": False
-      }
-   },
-   {
-      "_id": "5be0a05083ec9d4ba18f9d2a",
-      "CustID": 3,
-      "balance": "$2.36",
-      "phone": "+1 (854) 544-3144",
-      "Location": "British Columbia",
-      "Start": "2018-04-18T04:47:55",
-      "End": "2018-04-18T04:49:55",
-      "Event": {
-         "Voice": True,
-         "Data": False
-      }
-   },
-   {
-      "_id": "5be0a050bb367e94041f19b6",
-      "CustID": 4,
-      "balance": "$7.34",
-      "phone": "+1 (971) 416-2964",
-      "Location": "British Columbia",
-      "Start": "2018-06-24T02:50:03",
-      "End": "2018-06-24T02:57:57",
-      "Event": {
-         "Voice": True,
-         "Data": False
-      }
-   },
-   {
-      "_id": "5be0a0505e872429b7b037ca",
-      "CustID": 5,
-      "balance": "$10.06",
-      "phone": "+1 (993) 411-3848",
-      "Location": "Alberta",
-      "Start": "2018-12-04T02:49:03",
-      "End": "2018-12-04T02:59:03",
-      "Event": {
-         "Voice": True,
-         "Data": False
-      }
-   }
-]
+from kafka import KafkaProducer
+import datetime, time, random, threading
+import module as md
 
 
-class Producer(threading.Thread):
-    daemon = True
+rates = [1, 10, 100]  # messages per second
+sample_sigma = 500
+fuzz_sigma = 10
 
-    def run(self):
-        producer = KafkaProducer(bootstrap_servers=['199.60.17.210:9092', '199.60.17.193:9092'],
-                                 value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-
-        while True:
-            producer.send('cdr-10', json_file)
-            time.sleep(1)
-
-
-class Consumer(threading.Thread):
-    daemon = True
-
-    def run(self):
-        consumer = KafkaConsumer(bootstrap_servers=['199.60.17.210:9092', '199.60.17.193:9092'],
-                                 value_deserializer=lambda m: json.loads(m.decode('utf-8')))
-        consumer.subscribe(['cdr-10'])
-        for message in consumer:
-            print (message)
-
-
-def main():
-    threads = [
-        Producer(),
-        Consumer()
-    ]
-
-    for t in threads:
-        t.start()
-
-    time.sleep(10)
+def send_at(rate):
+    producer = KafkaProducer(bootstrap_servers=['199.60.17.210:9092', '199.60.17.193:9092'])
+    topic = 'cdr-' + str(rate)
+    interval = 1 / rate
+    while True:
+        msg = md.generate()
+        producer.send(topic, msg.encode('ascii'))
+        time.sleep(interval)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(
-        format='%(asctime)s.%(msecs)s:%(name)s:%(thread)d:' +
-               '%(levelname)s:%(process)d:%(message)s',
-        level=logging.INFO
-    )
-    main()
+    for rate in rates:
+        server_thread = threading.Thread(target=send_at, args=(rate,))
+        server_thread.setDaemon(True)
+        server_thread.start()
+
+    while 1:
+        time.sleep(1)
--- Project/module.py
+++ Project/module.py
@@ -1,37 +1,68 @@
-from json import dumps
-from faker import Faker
-import random
-import collections
+
+import random,time
 import yaml_loader as yam
-import json
+import csv
 
 
-def fake_person_generator(length, fake):
+class Customer:
+    'Common base class for all customer'
+    customer_count = 0
 
-    for x in range(length):  # xrange in Python 2.7
-        yield {'CustID': x,
-               'last_name': fake.last_name(),
-               'first_name': fake.first_name(),
-               'phone_number': fake.phone_number(),
-               'email': fake.email(),
-               'balance': "$"+str(round(random.uniform(0,100),2)),
-               'event': {"Voice": bool(random.getrandbits(1)), "Data": bool(random.getrandbits(1))}
-               }
+    def __init__(self, id, phone, email, location):
+        self.id = id
+        self.phone = phone
+        self.email = email
+        self.location = location
+        Customer.customer_count += 1
 
-def main():
-    filepath = "config.yaml"
-    data = yam.loader(filepath)
-    database = []
-    length = data.get('RECORDS')
-    filename = 'cdr' + str(length)
-    fake = Faker()  # <--- Forgot this
-    fpg = fake_person_generator(length, fake)
-    with open('%s.json' % filename, 'w') as output:
-        output.write('[')  # to made json file valid according to JSON format
-        for person in fpg:
-            json.dump(person, output)
-        output.write(']')  # to made json file valid according to JSON format
-    print("Done")
+    def displayCount(self):
+        print("Total Customer" + str(Customer.customer_count))
 
-if __name__ == "__main__":
-    main()
+    def displayCustomer(self):
+        print("ID : " + str(self.id) + " Phone: " + str(self.phone))
+
+def strTimeProp(start, end, format, prop):
+    stime = time.mktime(time.strptime(start, format))
+    etime = time.mktime(time.strptime(end, format))
+    ptime = stime + prop * (etime - stime)
+
+    return time.strftime(format, time.localtime(ptime))
+
+
+def randomDate(start, end, prop):
+    return strTimeProp(start, end, '%m/%d/%Y %I:%M %p', prop)
+
+def load_customer():
+    customer_list = dict()
+    with open('customer_list.csv', mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        line_count = 0
+        for row in csv_reader:
+            customer_list[line_count] = Customer(line_count, row["phone"], row["email"], row["location"])
+            line_count += 1
+    return customer_list
+
+def generate():
+    # filepath = "config.yaml"
+    # data = yam.loader(filepath)
+    # length = data.get('RECORDS')
+    length = 2
+    data = load_customer()
+    msg = ''
+    for i in range(1,length):
+        for j in data:
+            date = str(random.randint(1,12)) + "/" + str(random.randint(1,31))+"/2018 "
+            hour = random.randint(1,24)
+            minute = random.randint(1,60)
+            second = random.randint(1,60)
+            start_time = str(hour) + ":" + str(minute)+":" + str(second)
+            end_time =  str(hour) + ":" + str(minute+random.randint(1,second+10))+":" + str(random.randint(1,second+10))
+            msg += str(data[j].id) + ", " \
+                   + str(data[j].phone)+ ", " \
+                   + str(data[j].email) + ", " \
+                   + str(data[j].location) + ", " \
+                   + str("$"+str(round(random.uniform(0,20),2))) + ", " \
+                   + date + start_time + ", " \
+                   + date + end_time + '\n'
+    #print(msg)
+    return msg
